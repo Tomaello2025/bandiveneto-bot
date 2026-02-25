f2fda0b45b955bceeb668a0773c9d1b3701aaca8
Tomaello2025 <andrea.tomaello@tomaello.eu>
2026-02-25 14:45:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 from telegram import Bot
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
-CHANNEL_ID = "@bandiveneto"
+CHANNEL_ID = "@-1003752206217"
 
 URL = "https://bandi.regione.veneto.it/Public/Elenco?Tipo=1"
 
